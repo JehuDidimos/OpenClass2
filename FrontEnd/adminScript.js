68c557aab305b037804647c45fddd8a735dff954
JehuDidimos <jehudidimos.cs@gmail.com>
2025-08-06 16:56:33 -0500

    Add modal functionality

--- FrontEnd/adminScript.js
+++ FrontEnd/adminScript.js
@@ -1,36 +1,111 @@
 document.addEventListener("DOMContentLoaded", () => {
   checkToken();
+  getGallery();
   let editButton = document.querySelector(".edit-button");
   editButton.addEventListener("click", () => {
-    openDeleteModal();
+    openModal();
   });
 
-  let closeButton = document.querySelector('.btn-close');
-  closeButton.addEventListener('click', () => {
-    closeEditModal();
-  })
+  let closeButtons = document.querySelectorAll(".btn-close");
+  closeButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      closeEditModal();
+    });
+  });
 
-  let overlay = document.querySelector('.overlay');
-  overlay.addEventListener('click', () => {
+  let backButton = document.querySelector(".back-btn");
+  backButton.addEventListener("click", () => {
+    backToDelete();
+  });
+
+  let overlay = document.querySelector(".overlay");
+  overlay.addEventListener("click", () => {
     closeEditModal();
   });
 
-  let addButton = document.querySelector('.add-project-button');
-  addButton.addEventListener('click', () => {
+  let addButton = document.querySelector(".add-project-button");
+  addButton.addEventListener("click", () => {
     addProjectModal();
-  })
+  });
 
-  
+  imageInput = document.querySelector("#photo");
+  imageInput.addEventListener("change", () => {
+    const file = imageInput.files[0];
+
+    changeImagePreview(file);
+  });
+
+  let submitProject = document.querySelector(".submit-project-button");
+  submitProject.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log("CLICKED1");
+    submitForm();
+  });
 });
 
-getGallery();
+function changeImagePreview(file) {
+  if (file && file.type.startsWith("image/")) {
+    const reader = new FileReader();
+    let preview = document.querySelector(".preview-image");
+    reader.onload = function (e) {
+      preview.src = e.target.result;
+    };
 
-function addProjectModal(){
-  let deleteModal = document.querySelector('.delete-modal');
-  deleteModal.classList.add('hidden');
+    reader.onerror = function (err) {
+      console.error("FileReader error", err);
+    };
+    reader.readAsDataURL(file);
+    let previewArea = document.querySelector(".photo-input");
+    let inputArea = document.querySelector(".photo-button-wrapper");
+    previewArea.insertBefore(preview, inputArea);
+    inputArea.classList.add("hidden");
+  } else {
+    window.alert("Please select an image");
+  }
+}
 
-  let addModal = document.querySelector('.add-modal');
-  addModal.classList.remove('hidden');
+async function submitForm() {
+  let title = document.querySelector("#title");
+  let category = document.querySelector("#category");
+  let photo = document.querySelector("#photo");
+
+  console.log(title.value);
+  console.log(category.value);
+  console.log(photo.files[0]);
+
+  let formData = new FormData();
+  formData.append('image', photo.files[0]);
+  formData.append('title', `${title.value}`);
+  formData.append('category', `${category.value}`);
+
+  const url = "http://localhost:5678/api/works";
+  const request = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+    },
+    body: formData,
+  };
+
+  let response = await fetch(url, request).then(async (response) => {
+    console.log(response);
+  });
+}
+
+function backToDelete() {
+  let deleteModal = document.querySelector(".delete-modal");
+  let addModal = document.querySelector(".add-modal");
+
+  deleteModal.classList.remove("hidden");
+  addModal.classList.add("hidden");
+}
+
+function addProjectModal() {
+  let deleteModal = document.querySelector(".delete-modal");
+  deleteModal.classList.add("hidden");
+
+  let addModal = document.querySelector(".add-modal");
+  addModal.classList.remove("hidden");
 }
 
 function checkToken() {
@@ -68,20 +143,28 @@ function addEditStyling() {
   portfolioTarget.insertBefore(editDiv, galleryTarget);
 }
 
-function openDeleteModal() {
+function openModal() {
   let modal = document.querySelector(".modal-section");
   modal.classList.remove("hidden");
-  let deleteModal = document.querySelector('.delete-modal');
-  deleteModal.classList.remove('hidden');
-  let overlay = document.querySelector('.overlay');
-  overlay.classList.remove("hidden")
+
+  let deleteModal = document.querySelector(".delete-modal");
+  if (deleteModal.classList.contains("hidden")) {
+    deleteModal.classList.remove("hidden");
+  }
+
+  let overlay = document.querySelector(".overlay");
+  overlay.classList.remove("hidden");
 }
 
-function closeEditModal(){
+function closeEditModal() {
   let modal = document.querySelector(".modal-section");
   modal.classList.add("hidden");
-  let deleteModal = document.querySelector('.delete-modal');
-  deleteModal.classList.add('hidden');
+
+  let addModal = document.querySelector(".add-modal");
+  if (!addModal.classList.contains("hidden")) {
+    addModal.classList.add("hidden");
+  }
+
   let overlay = document.querySelector(".overlay");
   overlay.classList.add("hidden");
 }
@@ -123,45 +206,41 @@ function displayGallery(galleryList) {
     <img src="${item.imageUrl}" alt="${item.title}">
     `;
     modalElement.innerHTML = htmlModal;
-    modalElement.classList.add("modal-project")
-    modalElement.id = item.id
+    modalElement.classList.add("modal-project");
+    modalElement.id = item.id;
 
-    let deleteButton = document.createElement('button')
+    let deleteButton = document.createElement("button");
     deleteButton.innerHTML = `<i class="fa-solid fa-trash-can fa-xs"  style="color: #ffffff;"></i>`;
-    console.log(deleteButton)
-    deleteButton.addEventListener('click', (event) => {
+    deleteButton.addEventListener("click", (event) => {
       deleteProject(event.currentTarget.parentNode.id);
-    })
-    modalElement.appendChild(deleteButton)
+    });
+    modalElement.appendChild(deleteButton);
 
-    
     galleryDiv.appendChild(element);
     modalGallery.appendChild(modalElement);
-
-  
   }
 }
 
-async function deleteProject(id){
-  
-  try{
-    await fetch(`http://localhost:5678/api/works/${id}` ,{
-      method: 'DELETE',
+async function deleteProject(id) {
+  try {
+    await fetch(`http://localhost:5678/api/works/${id}`, {
+      method: "DELETE",
       headers: {
         "Content-type": "application/json",
-        "Authorization": `Bearer ${sessionStorage.getItem('token')}`
-      }
-    }).then( async (response) => {
-      if(response.status == 204){
-        let toDeleteProject = document.querySelectorAll(`figure#${CSS.escape(id)}`)
+        Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+      },
+    }).then(async (response) => {
+      if (response.status == 204) {
+        let toDeleteProject = document.querySelectorAll(
+          `figure#${CSS.escape(id)}`
+        );
         console.log(toDeleteProject);
         toDeleteProject.forEach((project) => {
           project.remove();
-        })
-
+        });
       }
     });
-  } catch (e){
-    console.error(e.message)
+  } catch (e) {
+    console.error(e.message);
   }
 }
